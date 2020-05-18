12fee1d083259464a47024248f8df2c977462d2a
Udit Jain <jain30udit@gmail.com>
2020-05-18 16:31:38 +0530

    Task Xml Added

--- app/src/main/java/udit/programmer/co/todos/MainActivity.kt
+++ app/src/main/java/udit/programmer/co/todos/MainActivity.kt
@@ -1,16 +1,40 @@
 package udit.programmer.co.todos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
+        floatinf_btn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                startActivity(Intent(this@MainActivity, TaskActivity::class.java))
+            }
+        })
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.history -> {
+                startActivity(Intent(this, HistoryActivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
--- app/src/main/java/udit/programmer/co/todos/ToDoDao.kt
+++ app/src/main/java/udit/programmer/co/todos/ToDoDao.kt
@@ -7,17 +7,17 @@ import androidx.room.Query
 
 @Dao
 interface ToDoDao {
-
-    @Insert
-    suspend fun insertTask(toDoModel: ToDoModel)
-
-    @Query("SELECT * FROM ToDoModel WHERE isFinished != -1")
-    suspend fun getTask(): LiveData<List<ToDoModel>>
-
-    @Query("UPDATE ToDoModel SET isFinished = 1 WHERE id = uid")
-    suspend fun finishTask(uid: Long)
-
-    @Query("DELETE FROM ToDoModel WHERE id = uid")
-    suspend fun deleteTask(uid: Long)
+//
+//    @Insert
+//    suspend fun insertTask(toDoModel: ToDoModel)
+//
+//    @Query("SELECT * FROM ToDoModel WHERE isFinished != -1")
+//    suspend fun getTask(): LiveData<List<ToDoModel>>
+//
+//    @Query("UPDATE ToDoModel SET isFinished = 1 WHERE id = uid")
+//    suspend fun finishTask(uid: Long)
+//
+//    @Query("DELETE FROM ToDoModel WHERE id = uid")
+//    suspend fun deleteTask(uid: Long)
 
 }
